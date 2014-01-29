caf59f4ac1c4d38874778f7d6d626566ffdd0321
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-01-29 12:12:53 -0800

    Added MedicalData System from other project
    Optimised imports

---
+++ samza-wikipedia/src/main/java/samza/examples/wikipedia/system/MedicalDataConsumer.java
@@ -0,0 +1,56 @@
+package samza.examples.wikipedia.system;
+
+import org.apache.samza.Partition;
+import org.apache.samza.system.IncomingMessageEnvelope;
+import org.apache.samza.system.SystemStreamPartition;
+import org.apache.samza.util.BlockingEnvelopeMap;
+import samza.examples.wikipedia.system.MedicalDataFeed.MedicalDataFeedEvent;
+import samza.examples.wikipedia.system.MedicalDataFeed.MedicalDataFeedListener;
+
+public class MedicalDataConsumer extends BlockingEnvelopeMap implements MedicalDataFeedListener
+{
+  private final String          systemName;
+  private final MedicalDataFeed feed;
+
+  public MedicalDataConsumer(final String systemName, final MedicalDataFeed feed)
+  {
+    this.systemName = systemName;
+    this.feed = feed;
+  }
+
+  public void onEvent(final MedicalDataFeedEvent event)
+  {
+    SystemStreamPartition systemStreamPartition =
+        new SystemStreamPartition(systemName, event.getInputFileName(), new Partition(0));
+
+    try
+    {
+      put(systemStreamPartition,
+          new IncomingMessageEnvelope(systemStreamPartition, null, null, event));
+    } catch (InterruptedException e)
+    {
+      // TODO
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void start()
+  {
+    feed.start();
+    feed.listen(this);
+  }
+
+  @Override
+  public void stop()
+  {
+    feed.unlisten(this);
+    feed.stop();
+  }
+
+  @Override
+  public void register(final SystemStreamPartition systemStreamPartition, final String startingOffset)
+  {
+    super.register(systemStreamPartition, startingOffset);
+  }
+}
