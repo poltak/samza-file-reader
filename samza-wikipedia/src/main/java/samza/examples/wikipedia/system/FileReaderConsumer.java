41e4473ca704e6cb6dca65d150921907fdfd4d20
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-02-04 21:58:06 -0800

    Reformated code to follow Samza project's Java formatting

--- samza-wikipedia/src/main/java/samza/examples/wikipedia/system/FileReaderConsumer.java
+++ samza-wikipedia/src/main/java/samza/examples/wikipedia/system/FileReaderConsumer.java
@@ -27,117 +27,102 @@ import java.io.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class FileReaderConsumer extends BlockingEnvelopeMap
-{
-  private final SystemStreamPartition systemStreamPartition;
-  private final Reader                fileReader;
-  private final BufferedReader        bufferedReader;
+public class FileReaderConsumer extends BlockingEnvelopeMap {
+    /**
+     * Threshold used to determine when there are too many IncomingMessageEnvelopes to be put onto the BlockingQueue.
+     */
+    private static final int BOUNDED_QUEUE_THRESHOLD = 100;
+    private final SystemStreamPartition systemStreamPartition;
+    private final Reader fileReader;
+    private final BufferedReader bufferedReader;
 
-  /**
-   * Sets up the SystemConsumer for reading from the specified file.
-   * @param systemName The name of this system.
-   * @param streamName The name of the stream upon which to place file contents.
-   * @param pathToInputFile The filesystem path to the file from which to read from.
-   * @throws FileNotFoundException Thrown if the <code>pathToInputFile</code> arg does not point to a readable file.
-   */
-  public FileReaderConsumer(final String systemName, final String streamName, final String pathToInputFile)
-      throws FileNotFoundException
-  {
-    this.systemStreamPartition = new SystemStreamPartition(systemName, streamName, new Partition(0));
-    this.fileReader = new FileReader(pathToInputFile);
-    this.bufferedReader = new BufferedReader(fileReader);
-  }
+    /**
+     * Sets up the SystemConsumer for reading from the specified file.
+     *
+     * @param systemName      The name of this system.
+     * @param streamName      The name of the stream upon which to place file contents.
+     * @param pathToInputFile The filesystem path to the file from which to read from.
+     * @throws FileNotFoundException Thrown if the <code>pathToInputFile</code> arg does not point to a readable file.
+     */
+    public FileReaderConsumer(final String systemName, final String streamName, final String pathToInputFile)
+            throws FileNotFoundException {
+        this.systemStreamPartition = new SystemStreamPartition(systemName, streamName, new Partition(0));
+        this.fileReader = new FileReader(pathToInputFile);
+        this.bufferedReader = new BufferedReader(fileReader);
+    }
 
-  @Override
-  public void register(final SystemStreamPartition systemStreamPartition, final String startingOffset)
-  {
-    super.register(systemStreamPartition, startingOffset);
-  }
+    @Override
+    public void register(final SystemStreamPartition systemStreamPartition, final String startingOffset) {
+        super.register(systemStreamPartition, startingOffset);
+    }
 
-  /**
-   * Constructs a new BufferedReader on the previously constructed FileReader and attempts to read in the input file.
-   * Reading from the file and placing the contents onto the SystemStreamPartition is done on a separate thread.
-   * If the file read is successful, setIsHead() is called to specify that the SystemStreamPartition has "caught up".
-   */
-  @Override
-  public void start()
-  {
-    Thread fileReadingThread = new Thread(
-      new Runnable()
-      {
-        @Override
-        public void run()
-        {
-          try
-          {
-            readInputFiles();
-            setIsAtHead(systemStreamPartition, true);
-          } catch (InterruptedException e)
-          {
-            e.getStackTrace();
-            stop();
-          }
+    /**
+     * Constructs a new BufferedReader on the previously constructed FileReader and attempts to read in the input file.
+     * Reading from the file and placing the contents onto the SystemStreamPartition is done on a separate thread.
+     * If the file read is successful, setIsHead() is called to specify that the SystemStreamPartition has "caught up".
+     */
+    @Override
+    public void start() {
+        Thread fileReadingThread = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            readInputFiles();
+                            setIsAtHead(systemStreamPartition, true);
+                        } catch (InterruptedException e) {
+                            e.getStackTrace();
+                            stop();
+                        }
+                    }
+                }
+        );
+
+        fileReadingThread.start();
+    }
+
+    /**
+     * Frees access to file when done.
+     */
+    @Override
+    public void stop() {
+        try {
+            this.bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-      }
-    );
-
-    fileReadingThread.start();
-  }
-
-  /**
-   * Frees access to file when done.
-   */
-  @Override
-  public void stop()
-  {
-    try
-    {
-      this.bufferedReader.close();
-    } catch (IOException e)
-    {
-      e.printStackTrace();
     }
-  }
 
-  /**
-   * Reads from the file in a BufferedReader, line-by-line, putting each line in an IncomingMessageEnvelope to be put
-   * onto the specified SystemStreamPartition.
-   *
-   * Calling from a separate thread is advised.
-   * @throws InterruptedException Thrown if System is interrupted while attempting to place file contents onto the
-   * specified SystemStreamPartition.
-   */
-  private void readInputFiles() throws InterruptedException
-  {
-    String line;
+    /**
+     * Reads from the file in a BufferedReader, line-by-line, putting each line in an IncomingMessageEnvelope to be put
+     * onto the specified SystemStreamPartition.
+     * <p/>
+     * Calling from a separate thread is advised.
+     *
+     * @throws InterruptedException Thrown if System is interrupted while attempting to place file contents onto the
+     *                              specified SystemStreamPartition.
+     */
+    private void readInputFiles() throws InterruptedException {
+        String line;
 
-    try
-    {
-      while ((line = bufferedReader.readLine()) != null)
-      {
-        put(systemStreamPartition, new IncomingMessageEnvelope(systemStreamPartition, null, null, line));
-      }
-    } catch (IOException e)
-    {
-      put(systemStreamPartition, new IncomingMessageEnvelope(systemStreamPartition, null, null,
-              "ERROR: Cannot read from input file:\n" + e.getMessage()));
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                put(systemStreamPartition, new IncomingMessageEnvelope(systemStreamPartition, null, null, line));
+            }
+        } catch (IOException e) {
+            put(systemStreamPartition, new IncomingMessageEnvelope(systemStreamPartition, null, null,
+                    "ERROR: Cannot read from input file:\n" + e.getMessage()));
+        }
     }
-  }
 
-
-  /**
-   * Threshold used to determine when there are too many IncomingMessageEnvelopes to be put onto the BlockingQueue.
-   */
-  private static final int BOUNDED_QUEUE_THRESHOLD = 100;
-
-  /**
-   * Constructs a new bounded BlockingQueue of IncomingMessageEnvelopes. The bound is determined by the
-   * <code>BOUNDED_QUEUE_THRESHOLD</code> constant.
-   * @return A bounded queue used for queueing IncomingMessageEnvelopes to be sent to their specified destinations.
-   */
-  @Override
-  protected BlockingQueue<IncomingMessageEnvelope> newBlockingQueue()
-  {
-    return new LinkedBlockingQueue<IncomingMessageEnvelope>(BOUNDED_QUEUE_THRESHOLD);
-  }
+    /**
+     * Constructs a new bounded BlockingQueue of IncomingMessageEnvelopes. The bound is determined by the
+     * <code>BOUNDED_QUEUE_THRESHOLD</code> constant.
+     *
+     * @return A bounded queue used for queueing IncomingMessageEnvelopes to be sent to their specified destinations.
+     */
+    @Override
+    protected BlockingQueue<IncomingMessageEnvelope> newBlockingQueue() {
+        return new LinkedBlockingQueue<IncomingMessageEnvelope>(BOUNDED_QUEUE_THRESHOLD);
+    }
 }
--- samza-wikipedia/src/main/java/samza/examples/wikipedia/system/FileReaderSystemFactory.java
+++ samza-wikipedia/src/main/java/samza/examples/wikipedia/system/FileReaderSystemFactory.java
@@ -21,46 +21,46 @@ package samza.examples.wikipedia.system;
 import org.apache.samza.SamzaException;
 import org.apache.samza.config.Config;
 import org.apache.samza.metrics.MetricsRegistry;
-import org.apache.samza.system.*;
+import org.apache.samza.system.SystemAdmin;
+import org.apache.samza.system.SystemConsumer;
+import org.apache.samza.system.SystemFactory;
+import org.apache.samza.system.SystemProducer;
 import org.apache.samza.util.SinglePartitionSystemAdmin;
 
 import java.io.FileNotFoundException;
 
-public class FileReaderSystemFactory implements SystemFactory
-{
-  /** Determines the name of the stream upon which the file contents will be placed. */
-  private static final String OUTPUT_STREAM_NAME = "file-contents";
+public class FileReaderSystemFactory implements SystemFactory {
+    /**
+     * Determines the name of the stream upon which the file contents will be placed.
+     */
+    private static final String OUTPUT_STREAM_NAME = "file-contents";
 
-  /**
-   * Gets SystemConsumers for the MedicalData system.
-   * @param systemName The name of the System.
-   * @param config Provides access to the properties file where variables for the System may be defined.
-   * @return New SystemConsumer that converts the file contents into a Samza-compatible stream.
-   */
-  @Override
-  public SystemConsumer getConsumer(final String systemName, final Config config, final MetricsRegistry metricsRegistry)
-  {
-    String pathToInputFile = config.get("systems." + systemName + ".inputpath");
+    /**
+     * Gets SystemConsumers for the MedicalData system.
+     *
+     * @param systemName The name of the System.
+     * @param config     Provides access to the properties file where variables for the System may be defined.
+     * @return New SystemConsumer that converts the file contents into a Samza-compatible stream.
+     */
+    @Override
+    public SystemConsumer getConsumer(final String systemName, final Config config, final MetricsRegistry metricsRegistry) {
+        String pathToInputFile = config.get("systems." + systemName + ".inputpath");
 
-    try
-    {
-      return new FileReaderConsumer(systemName, OUTPUT_STREAM_NAME, pathToInputFile);
-    } catch (FileNotFoundException e)
-    {
-      e.printStackTrace();
-      return null;
+        try {
+            return new FileReaderConsumer(systemName, OUTPUT_STREAM_NAME, pathToInputFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-  }
 
-  @Override
-  public SystemProducer getProducer(final String systemName, final Config config, final MetricsRegistry metricsRegistry)
-  {
-    throw new SamzaException("Cannot produce to read only data.");
-  }
+    @Override
+    public SystemProducer getProducer(final String systemName, final Config config, final MetricsRegistry metricsRegistry) {
+        throw new SamzaException("Cannot produce to read only data.");
+    }
 
-  @Override
-  public SystemAdmin getAdmin(final String systemName, final Config config)
-  {
-    return new SinglePartitionSystemAdmin();
-  }
+    @Override
+    public SystemAdmin getAdmin(final String systemName, final Config config) {
+        return new SinglePartitionSystemAdmin();
+    }
 }
